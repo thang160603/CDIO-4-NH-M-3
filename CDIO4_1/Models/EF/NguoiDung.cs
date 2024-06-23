a97d2d61e0bc4c1d179dc61e0529e8e3e62717ae
NguyenHoangHai2210 <166629532+NguyenHoangHai2210@users.noreply.github.com>
2024-06-23 08:34:23 +0700

    Update NguoiDung.cs

--- CDIO4_1/Models/EF/NguoiDung.cs
+++ CDIO4_1/Models/EF/NguoiDung.cs
@@ -9,6 +9,8 @@ namespace CDIO_4._1.Models.EF
     [Table("nguoi_dung")]
     public class NguoiDung
     {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int ma_nguoi_dung { get; set; }
         public string ten_dang_nhap { get; set; }
         public string mat_khau { get; set; }    
