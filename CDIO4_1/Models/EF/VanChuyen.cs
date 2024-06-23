388f8b5478a1f4067f853f6de900170aaaa2e756
NguyenHoangHai2210 <166629532+NguyenHoangHai2210@users.noreply.github.com>
2024-06-23 08:35:29 +0700

    Update VanChuyen.cs

--- CDIO4_1/Models/EF/VanChuyen.cs
+++ CDIO4_1/Models/EF/VanChuyen.cs
@@ -9,6 +9,8 @@ namespace CDIO_4._1.Models.EF
     [Table("van_chuyen")]
     public class VanChuyen
     {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int ma_van_chuyen { get; set; }
         public string ten_van_chuyen { get; set; }
         public decimal phi_van_chuyen { get; set; }
