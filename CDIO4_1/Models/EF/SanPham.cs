423f7d83eb6b8ef51e555854c51ad25a43ec8f8b
NguyenHoangHai2210 <166629532+NguyenHoangHai2210@users.noreply.github.com>
2024-06-23 08:35:11 +0700

    Update SanPham.cs

--- CDIO4_1/Models/EF/SanPham.cs
+++ CDIO4_1/Models/EF/SanPham.cs
@@ -9,6 +9,8 @@ namespace CDIO_4._1.Models.EF
     [Table("san_pham")]
     public class SanPham
     {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int ma_san_pham { get; set; }
         public string ten_san_pham { get; set; }
         public string mo_ta { get; set; }
