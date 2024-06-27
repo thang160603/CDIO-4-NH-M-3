6d21d969e826bca6779f76a4b481bd0fad77e957
NguyenHoangHai2210 <166629532+NguyenHoangHai2210@users.noreply.github.com>
2024-06-27 22:47:47 +0700

    create Danhmuc controller

---
+++ CDIO-4.1/Areas/Admin/Controllers/DanhMucController.cs
@@ -0,0 +1,24 @@
+﻿using CDIO_4._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CDIO_4._1.Areas.Admin.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        ApplicationDbContext db =new ApplicationDbContext();
+        // GET: Admin/DanhMuc
+        public ActionResult Index()
+        {
+            var items = db.DanhMucs;
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();  
+        }
+    }
+}
